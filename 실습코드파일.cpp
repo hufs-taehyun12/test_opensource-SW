ec785e531f74168856b0a1a5ded173d462ae343e
hufs-taehyun12 <163493651+hufs-taehyun12@users.noreply.github.com>
2024-03-15 21:52:25 +0900

    Add files via upload

---
+++ 실습코드파일.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+class Coord_2D {
+private:
+	int x;
+	int y;
+
+public:
+	Coord_2D(int _x, int _y) :x(_x), y(_y) {}
+	void showCoord();
+
+	Coord_2D addCoord(Coord_2D& _k);
+	Coord_2D operator+(Coord_2D& _k);
+	bool operator==(Coord_2D& _k);
+	bool operator!=(Coord_2D& _k);
+	Coord_2D& operator++();
+	Coord_2D operator++(int);
+};
+
+Coord_2D Coord_2D::operator+(Coord_2D& _k) {
+	Coord_2D temp(x + _k.x, y + _k.y);
+	return temp;
+}
+Coord_2D Coord_2D::addCoord(Coord_2D& _k) {
+	Coord_2D temp(x + _k.x, y + _k.y);
+	return temp;
+}
+bool Coord_2D::operator==(Coord_2D& _k) {
+	//return ((x == _k.x) && (y == _k.y));
+	return !(*this != _k);
+}
+
+bool Coord_2D::operator!=(Coord_2D& _k) {
+	//return !((x == _k.x) && (y == _k.y));
+	return !(*this == _k);
+}
+Coord_2D& Coord_2D::operator++() {
+	x++;
+	y++;
+	return *this;
+}
+Coord_2D Coord_2D::operator++(int) {
+	Coord_2D temp = *this;
+	x++;
+	y++;
+	return temp;
+}
+
+void Coord_2D::showCoord() {
+	cout << "(" << x << "," << y << ")" << endl;
+}
+
+int main() {
+	Coord_2D a(10, 10);
+	//Coord_2D b(20, 20);
+
+	a.showCoord();
+	//b.showCoord();
+
+	//++(++a);
+	//a.showCoord();
+	Coord_2D b = a++;
+	a.showCoord();
+	b.showCoord();
+
+	return 0;;
+}
